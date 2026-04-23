26b3e747afe65d2160723204ea9c454deb26f478
Jeka <odinked@gmail.com>
2026-04-23 13:38:57 +0300

    feat(contact): intro + 50/50 form+map, contacts moved to CtaFooter

--- app/page/Contact.jsx
+++ app/page/Contact.jsx
@@ -1,70 +1,28 @@
 "use client";
 import React from "react";
-import styles from "@/app/styles/page/contact.module.css";
-import Image from "next/image";
 import { useTranslations } from "next-intl";
+import PageIntro from "@/app/components/PageIntro/PageIntro";
 import Form from "@/app/components/form/Form";
-import Googlmap from "@/app/components/googlmaps/Googlmap"
+import Googlmap from "@/app/components/googlmaps/Googlmap";
+import styles from "@/app/styles/page/contact.module.css";
 
 function Contact() {
   const t = useTranslations("Index");
   return (
     <>
-      <div className={styles.contact}>
-      <Googlmap/>
+      <PageIntro
+        caps={t("contactFooter.caps")}
+        title={t("contactus")}
+      />
 
-    <div className={styles.contact__block}>
-
-        <div className={styles.contact__info}>
-          <ul>
-              <li className={styles.sub__contact__info}><h2>{t('contactus')}</h2></li>
-            
-            <li>{t('manager')}:+38 097 136-20-00 </li>
-            <li>{t('director')}:+38 067 674-58-43 </li>
-            <li>
-              <a    rel="preload" href="mailto:zubrzubr2000@gmail.com ">
-                zubrzubr2000@gmail.com
-              </a>
-            </li>
-            <li>
-              <a    rel="preload" href="mailto:gremroman@gmail.com">gremroman@gmail.com</a>
-            </li>
-            <li>
-              <a   rel="preload" href="mailto:grem_roman@icloud.com">
-                grem_roman@icloud.com{" "}
-              </a>
-            </li>
-          
-          </ul>
+      <section className={styles.formMap}>
+        <div className={styles.formWrap} id="contact_name">
+          <Form />
         </div>
-        <div className={styles.contact__info}>
-          <ul>
-           
-              <li className={styles.sub__contact__info}><h2>{t('findus')}</h2></li>
-           
-            <li>79000</li>
-            <li>{t("ukrain")}</li>
-                <li>{t("lviv")}</li>
-                <li>{t("street")}</li>
-          </ul>
+        <div className={styles.mapWrap}>
+          <Googlmap />
         </div>
-        <div className={styles.contact__info}>
-          <a    rel="preload" href="https://derevyna.fordaq.com/fordaq/srvFordaqReport/UkrWood+LLC_642294.html">
-            <h2>FORDAQ.COM</h2>
-            <Image
-              src="/fordaq-logo-02.webp"
-              width={190}
-              height={100}
-              alt="fordaq"
-            />
-          </a>
-        </div>
-      </div>
-              </div>
-      <div id='contact_name' className={styles.contact__form}>
-        <Form />
-      </div>
-    
+      </section>
     </>
   );
 }
